e7faff2802042aaad61cb069f727b58891ef54fc
Andy Smith <andy@ajsmith.us>
2019-04-09 22:42:36 -0400

    Specify which albums to download

--- README.md
+++ README.md
@@ -5,11 +5,30 @@ Download all of the pictures from a SmugMug user, including password-protected u
 * Install Python 3
 * Clone this repository or download it as a zip
 * Install the requirements:  `pip install -r requirements.txt`
+* NOTE: If you are on Linux, you will probably need to type `pip3` and `python3` instead
 
 ## Usage
 * Run `python smdl.py -u USERNAME` and it will begin downloading your pictures into separate folders in the default output directory. The username is what is found in the URL, i.e. USERNAME.smugmug.com
 * To specify the output directory, use the `-o` flag: `python smdl.py -u USERNAME -o output`
 * If the user requires an unlock password, you must sign in using the password in your web browser, then copy over the SMSESS cookie. In Google Chrome, you can view your cookies by pressing `Ctrl-Shift-I`, then go to the Application tab, go to the  Cookies dropdown, and click on https://USERNAME.smugmug.com. Then copy the value of the SMSESS cookie. You can then paste this cookie as an argument: `python smdl.py -u USERNAME -s 
 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx`
+* For a full list of command-line options, run `python smdl.py -h`, or see below:
+```
+usage: smdl.py [-h] [-s SESSION] -u USER [-o OUTPUT] [--albums ALBUMS]
+
+SmugMug Downloader
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -s SESSION, --session SESSION
+                        session ID (required if user is password protected);
+                        log in on a web browser and paste the SMSESS cookie
+  -u USER, --user USER  username (from URL, USERNAME.smugmug.com)
+  -o OUTPUT, --output OUTPUT
+                        output directory
+  --albums ALBUMS       specific album names to download, split by $. Defaults
+                        to all. (e.g. --albums "Title 1$Title 2$Title 3")
+```
+
 
 ###### NOTICE: SmugMug is a registered trademark of SmugMug Inc. This repository is not affiliated with SmugMug, Inc.
--- smdl.py
+++ smdl.py
@@ -9,13 +9,18 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 parser = argparse.ArgumentParser(description="SmugMug Downloader")
-parser.add_argument("-s", "--session", help="session ID (required if user is password protected); log in on a web browser and paste the SMSESS cookie")
-parser.add_argument("-u", "--user", help="username (from URL, USERNAME.smugmug.com)", required=True)
-parser.add_argument("-o", "--output", default="output/", help="output directory")
+parser.add_argument(
+	"-s", "--session", help="session ID (required if user is password protected); log in on a web browser and paste the SMSESS cookie")
+parser.add_argument(
+	"-u", "--user", help="username (from URL, USERNAME.smugmug.com)", required=True)
+parser.add_argument("-o", "--output", default="output/",
+                    help="output directory")
+parser.add_argument(
+	"--albums", help='specific album names to download, split by $. Defaults to all. (e.g. --albums "Title 1$Title 2$Title 3")')
 
 args = parser.parse_args()
 
-endpoint = "https://smugmug.com"
+endpoint = "https://www.smugmug.com"
 
 # Session ID (required if user is password protected)
 # Log in on a web browser and copy the SMSESS cookie
@@ -28,12 +33,17 @@ if args.output[-1:] != "/" and args.output[-1:] != "\\":
 else:
 	output_dir = args.output
 
+if args.albums:
+	specificAlbums = [x.strip() for x in args.albums.split('$')]
+
+
 # Gets the JSON output from an API call
 def get_json(url):
 	r = requests.get(endpoint + url, cookies=cookies)
 	soup = BeautifulSoup(r.text, "html.parser")
 	for pre in soup.find_all("pre"):
 		return json.loads(pre.text)
+
 
 # Retrieve the list of albums
 print("Downloading album list...", end="")
@@ -49,6 +59,8 @@ except KeyError:
 # Create output directories
 print("Creating output directories...", end="")
 for album in albums["Response"]["AlbumList"]:
+	if album["Name"].strip() not in specificAlbums:
+		continue
 	directory = output_dir + album["UrlPath"][1:]
 	if not os.path.exists(directory):
 		os.makedirs(directory)
@@ -56,6 +68,11 @@ print("done.")
 
 # Loop through each album
 for album in tqdm(albums["Response"]["AlbumList"]):
+	print(album)
+	if args.albums:
+		if album["Name"].strip() not in specificAlbums:
+			continue
+
 	album_path = output_dir + album["UrlPath"][1:]
 	images = get_json(album["Uri"] + "!images")
 
@@ -67,15 +84,16 @@ for album in tqdm(albums["Response"]["AlbumList"]):
 
 		# Loop through each image in the album
 		for image in tqdm(images["Response"]["AlbumImage"]):
-			image_path = album_path + "/" + re.sub('[^\w\-_\. ]', '_', image["FileName"])
-			
+			image_path = album_path + "/" + \
+				re.sub('[^\w\-_\. ]', '_', image["FileName"])
+
 			# Skip if image has already been saved
 			if os.path.isfile(image_path):
 				continue
-			
+
 			image_req = get_json(image["Uris"]["LargestImage"]["Uri"])
 			download_url = image_req["Response"]["LargestImage"]["Url"]
-			
+
 			try:
 				urllib.request.urlretrieve(download_url, image_path)
 			except UnicodeEncodeError as ex:
